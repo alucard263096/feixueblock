a9bc869b18d3fc4418a010b8d43cb51362b69b6d
steve.cai <alucard263096@126.com>
2018-04-11 21:58:16 +0800

    qianru

--- source/apis/livemeeting.api.js
+++ source/apis/livemeeting.api.js
@@ -134,6 +134,38 @@ export class LivemeetingApi {
           ApiConfig.CloseLoading();
       }
     })
+  }
+
+
+  up(json, callback, showLoading = true) {
+
+    if (showLoading)
+      ApiConfig.ShowLoading();
+
+    var header = ApiConfig.GetHeader();
+    console.log(header);
+    wx.request({
+      url: ApiConfig.GetApiUrl() + 'livemeeting/up',
+      data: json,
+      method: 'POST',
+      dataType: 'json',
+      header: header,
+      success: function (res) {
+        if (callback != null) {
+          callback(res.data);
+        }
+      },
+      fail: function (res) {
+        console.log(res);
+        callback(false);
+      },
+      complete: function (res) {
+        console.log(res);
+
+        if (showLoading)
+          ApiConfig.CloseLoading();
+      }
+    })
   } 
 }
 
