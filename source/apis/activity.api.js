a45df53b85f23aee68a66f5522fb7c6c1115e41c
steve.cai <alucard263096@126.com>
2018-05-22 21:10:37 +0800

    commit

---
+++ source/apis/activity.api.js
@@ -0,0 +1,45 @@
+
+        /****使用方法，下面两句复制到page的js文件的头部
+		
+import { ApiConfig } from '../../apis/apiconfig';
+import { ServiceApi } from '../../apis/service.api';
+
+var serviceApi=new ServiceApi();
+        *******/
+import { ApiConfig } from 'apiconfig';
+export class ActivityApi
+{
+			//获取相关的服务
+				list(json, callback, showLoading = true) {
+
+					if (showLoading)
+					ApiConfig.ShowLoading();
+    
+					var header=ApiConfig.GetHeader();
+					console.log(header);
+					wx.request({
+					  url: ApiConfig.GetApiUrl() + 'activity/list',
+					  data: json,
+					  method: 'POST',
+					  dataType: 'json',
+					  header: header,
+					  success: function (res) {
+						if (callback != null) {
+						  callback(res.data);
+						}
+					  },
+					  fail: function (res) {
+						console.log(res);
+						callback(false);
+					  },
+					  complete: function (res) {
+						console.log(res);
+
+						if (showLoading)
+						ApiConfig.CloseLoading();
+					  }
+					})
+				  }
+                
+}
+
