02563b4cedf30f01d750e728a9d11184a3b5df76
steve.cai <alucard263096@126.com>
2018-05-24 21:38:06 +0800

    commit

--- source/apis/activity.api.js
+++ source/apis/activity.api.js
@@ -72,5 +72,37 @@ export class ActivityApi
             }
           })
         }
+        //获取相关的服务
+        submitticket(json, callback, showLoading = true) {
+
+          if (showLoading)
+            ApiConfig.ShowLoading();
+
+          var header = ApiConfig.GetHeader();
+          console.log(header);
+          wx.request({
+            url: ApiConfig.GetApiUrl() + 'activity/submitticket',
+            data: json,
+            method: 'POST',
+            dataType: 'json',
+            header: header,
+            success: function (res) {
+              if (callback != null) {
+                callback(res.data);
+              }
+            },
+            fail: function (res) {
+              console.log(res);
+              callback(false);
+            },
+            complete: function (res) {
+              console.log(res);
+
+              if (showLoading)
+                ApiConfig.CloseLoading();
+            }
+          })
+        }
 }
 
+ 
--- source/pages/activitybuyinfo/activitybuyinfo.js
+++ source/pages/activitybuyinfo/activitybuyinfo.js
@@ -10,7 +10,12 @@ class Content extends AppBase {
   }
   onLoad(options) {
     this.Base.Page = this;
+    options.ticket_id=1;
+    options.ticket_buycount=3;
     super.onLoad(options);
+    wx.hideShareMenu({
+      
+    })
   }
   onShow() {
     var that = this;
@@ -29,6 +34,37 @@ class Content extends AppBase {
   }
   submitForm(e){
     console.log(e);
+    var that=this;
+    var json=e.detail.value;
+    super.checkRealname(function () {
+      if(json.company.trim()==""){
+        that.Base.info("请输入公司名称");
+        return;
+      }
+      if (json.position.trim() == "") {
+        that.Base.info("请输入职位");
+        return;
+      }
+      if (json.wechataccount.trim() == "") {
+        that.Base.info("请输入微信号");
+        return;
+      }
+      if (json.industry.trim() == "") {
+        that.Base.info("请输入所在行业");
+        return;
+      }
+      json.activity_ticket_level_id=that.Base.options.ticket_id;
+      json.buycount=that.Base.options.ticket_buycount;
+      var api = new ActivityApi();
+      api.submitticket(json, (ret) => {
+        if(ret.code==0){
+          
+        }else{
+          that.Base.info(ret.return);
+        }
+      });
+
+    })
   }
 }
 var content = new Content();
